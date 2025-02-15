328865223e81bd45bded06d47b941ba63c54083d
Acer <nami06maria@gmail.com>
2025-02-15 20:33:30 +0530

    Added Functions with Outputs

---
+++ 1) Beginner/9. Functions with Outputs/ii) calculator.py
@@ -0,0 +1,57 @@
+"""
+    A simple calculator to do simple operations.
+    Example:
+        First Number: 5
+        +
+        -
+        *
+        /
+        Pick an operation: -
+        What's the next number?: 3
+        5.0 - 3.0 = 2.0
+
+"""
+
+from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("First Number: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
